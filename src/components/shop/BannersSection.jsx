914188780d9a2a569241fc2a07f02d8ee54c5ba9
Ehsan <ehsannajaar@gmail.com>
2025-07-29 17:59:38 +0330

    change BannerSection components to CSR

--- src/components/shop/BannersSection.jsx
+++ src/components/shop/BannersSection.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -5,7 +7,7 @@ export default function BannersSection() {
   return (
     <div className="flex flex-col lg:flex-row gap-4 px-4 md:px-12 lg:px-24 md:pt-10 lg:pt-12">
       {/* big banner */}
-      <section className="w-full lg:w-4/5 relative h-[220px] sm:h-[280px] md:h-[350px] lg:h-[510px]">
+      <section className="w-full lg:w-4/5 relative min-h-[510px] h-[220px] sm:h-[280px] md:h-[350px] lg:h-[510px]">
         <Image
           src="/images/banners/main-banner.jpg"
           alt="بنر اصلی"
@@ -17,7 +19,6 @@ export default function BannersSection() {
 
       {/* small banners */}
       <section className="w-full lg:w-1/5 flex flex-row lg:flex-col gap-4 h-auto lg:h-[510px]">
-        {/* بنر اندرویدی */}
         <div className="relative w-1/2 lg:w-full h-[120px] sm:h-[150px] md:h-[180px] lg:h-1/2">
           <Link href="/shop/type/اندرویدی">
             <Image
@@ -25,12 +26,10 @@ export default function BannersSection() {
               alt="بنر اندرویدی"
               fill
               className="rounded-lg object-cover"
-              priority
             />
           </Link>
         </div>
 
-        {/* بنر سیار */}
         <div className="relative w-1/2 lg:w-full h-[120px] sm:h-[150px] md:h-[180px] lg:h-1/2">
           <Link href="/shop/type/سیار">
             <Image
@@ -38,7 +37,6 @@ export default function BannersSection() {
               alt="بنر سیار"
               fill
               className="rounded-lg object-cover"
-              priority
             />
           </Link>
         </div>
